16b64f5e974eac9159a35b508905f5bc39a3a9ca
Igor Nemkovich <to.nemkovich@gmail.com>
2021-08-03 18:11:41 +0300

    16.jenkins_start

--- 16.Jenkins_start/master/bootstrap.sh
+++ 16.Jenkins_start/master/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 yum install java wget git -y --nogpgcheck -q
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
